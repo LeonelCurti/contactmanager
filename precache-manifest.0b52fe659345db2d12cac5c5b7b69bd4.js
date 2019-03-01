56d85ba3d1c5faf41c58e7327efec835f800f077
Leonel Curti <leonelcurti@gmail.com>
2019-02-28 22:15:35 -0300

    Updates

--- precache-manifest.0b52fe659345db2d12cac5c5b7b69bd4.js
+++ precache-manifest.0b52fe659345db2d12cac5c5b7b69bd4.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
   },
   {
-    "revision": "76fbf3d602de8f41698a",
-    "url": "/contactmanager/static/js/main.76fbf3d6.chunk.js"
+    "revision": "c2d72c6dd0e6687197ed",
+    "url": "/contactmanager/static/js/main.c2d72c6d.chunk.js"
   },
   {
     "revision": "c8f823d124e73a84ec4a",
     "url": "/contactmanager/static/js/2.c8f823d1.chunk.js"
   },
   {
-    "revision": "76fbf3d602de8f41698a",
+    "revision": "c2d72c6dd0e6687197ed",
     "url": "/contactmanager/static/css/main.f301b86d.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/css/2.c131e700.chunk.css"
   },
   {
-    "revision": "e1faeeec8791c0bd5af3c68194d1cfa2",
+    "revision": "25f3ddd4d70f5d9ff41659d1b72c3cee",
     "url": "/contactmanager/index.html"
   }
 ];
